94bfa780f47cc2935b9e49ad970ccb5c62ac2357
Ray <raymond.dee@ontariotechu.net>
2019-12-19 20:08:53 -0500

    [MOD] Cleaned project.

---
+++ TEMPLE/Def.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+enum GameState;
+
+GameState menu();
+
+GameState theMoves();
+
+GameState roomEvent();
+
+GameState gameOver();
+
+GameState winning();
+
+void WindowSize();
